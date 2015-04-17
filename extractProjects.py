4ba81c60adb0dadea5e0fc8506abf22f1dc83543
Alex Kindel <atkindel@stanford.edu>
2015-04-17 15:39:00 -0700

    In extractProjects.py, script now produces dict of project data fields.

--- extractProjects.py
+++ extractProjects.py
@@ -2,23 +2,54 @@ from pypodio2 import api
 import json
 import getpass
 import ConfigParser
-import readline
+import sys
+
+
+def user_input(prompt):
+    '''
+    Wrapper for raw_input with prompt text written to stderr
+    @param prompt: Prompt to echo in terminal
+    @type prompt: String
+    '''
+    sys.stderr.write(prompt)
+    return raw_input()
+
+
+def parseFields():
+    '''
+    TODO: write docstring
+    '''
+
+
+
+# Driver for extracting projects. Pulls all items from Podio project
+# application and extracts key-value pairs matching external_id to
+# value based on field_type.
 
 # Get API credentials
 config = ConfigParser.RawConfigParser()
 config.read('key.cfg')
-app = config.get('APIKey', 'app')
-key = config.get('APIKey', 'key')
+etl = config.get('APIKey', 'etl') # api app id
+key = config.get('APIKey', 'key') # api key
+app = config.get('APIKey', 'app') # podio internal app id
 
 # Get user credentials
-username = raw_input('Username: ')
+username = user_input('Username: ')
 password = getpass.getpass('Password: ')
 
-# Pull X projects from Podio
-x = 25
-c = api.OAuthClient(app, key, username, password)
-dict = c.Application.get_items(app_id = 2589515, limit = x)
+# Extract projects from Podio into dict
+x = int(user_input('How many projects to pull?: '))
+c = api.OAuthClient(etl, key, username, password)
+projs = c.Application.get_items(app_id = app, limit = x)
 
-# Pretty print JSON output for review
-dictpp = json.dumps(obj = dict, sort_keys=True, indent = 4, separators = (',', ': '))
-print dictpp
+# Map field ID to type in dictionary across projects
+fields = dict()
+for proj in projs['items']:
+    for field in proj['fields']:
+        fields[field['external_id']] = field['type']
+
+# Print all field IDs and corresponding types
+for f in fields:
+    print "%s: %s" % (f, fields[f])
+
+print fields.values()
--- getProj.py
+++ getProj.py
@@ -6,16 +6,17 @@ import ConfigParser
 # Get API credentials
 config = ConfigParser.RawConfigParser()
 config.read('key.cfg')
-app = config.get('APIKey', 'app')
-key = config.get('APIKey', 'key')
+etl = config.get('APIKey', 'etl') # api app id
+key = config.get('APIKey', 'key') # api key
+app = config.get('APIKey', 'app') # podio internal app id
 
 # Get user credentials
-username = raw_input("Username: ")
-password = getpass.getpass("Password: ")
+username = raw_input('Username: ')
+password = getpass.getpass('Password: ')
 
 # Extract sample item for analysis
-c = api.OAuthClient(app, key, username, password)
-s = c.Item.find(265515261)
+c = api.OAuthClient(etl, key, username, password)
+s = c.Item.find(item_id = 36445119)
 
 # Pretty print JSON output for review
 spp = json.dumps(obj = s, sort_keys=True, indent = 4, separators = (',', ': '))
